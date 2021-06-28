28f6b3615e846e70a99f37f65d178a62de46f952
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2021-06-28 10:44:39 -0400

    Replace initial project name with placeholder variable
    Details
    - Replace instances of `django_base_template` with `{{ project_name }}`
    
    Testing
    - N/A

--- django_base_template/urls.py
+++ django_base_template/urls.py
@@ -1,4 +1,4 @@
-"""django_base_template URL Configuration
+"""{{ project_name }} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
