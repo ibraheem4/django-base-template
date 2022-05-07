63da27d8e9d16b5232fc52a0b67b82cd01a8028b
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2022-05-07 08:43:03 -0400

    build: update django-allauth
    use re_path instead of deprecated django.conf.urls.url()

--- project_name/urls.py
+++ project_name/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import re_path as url
+from django.conf.urls import include
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
