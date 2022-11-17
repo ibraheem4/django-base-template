7b83c25de9dd33c1b6d84d65dac15948158aacb3
Ibraheem Abdul-Malik <ibraheem4@gmail.com>
2022-11-17 11:02:23 -0500

    fix: linting errors (import sort order)

--- apps/todos/views.py
+++ apps/todos/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
-from apps.todos.serializers import TodoSerializer
-from apps.todos.models import Todo
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework_json_api.parsers import JSONParser as JSONAPIParser
 from rest_framework_json_api.renderers import JSONRenderer as JSONAPIRenderer
+from apps.todos.serializers import TodoSerializer
+from apps.todos.models import Todo
 
 
 class TodoViewSet(viewsets.ModelViewSet):  # pylint: disable=too-many-ancestors
